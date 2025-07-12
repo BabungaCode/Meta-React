9a648eebe4c2ae3ddca4102c24a0ad34566a718d
babungacode <babungagamer@gmail.com>
2025-07-12 09:04:11 -0700

    module content added

---
+++ scripts/meta-react.js
@@ -0,0 +1,85 @@
+let socket;
+let module;
+
+class Status extends Application {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      id: "elt-status",
+      title: "Messages",
+      template: `modules/meta-react/templates/status.hbs`,
+      width: 300,
+      height: 100,
+      top: 80,
+      left: 130,
+      resizable: false,
+      minimizable: false,
+    });
+  }
+}
+
+Hooks.once("init", () => {
+  console.log("Meta React | Initialising the module.");
+  module = game.modules.get("meta-react");
+  module.status = new Status();
+  module.status.render(true);
+});
+
+const getCurrentUser = () => game.users.current;
+
+const sendApproval = (approval) => {
+  const user = getCurrentUser();
+  const name = canvas?.tokens?.controlled?.[0]?.name ?? user?.character?.name ?? user.name
+  socket.executeForEveryone("approval", name, approval);
+};
+
+const cleanWindowClass = (application) => {
+  const app = application.element[0];
+  app.style.boxShadow = "none";
+  app.style.background = "none";
+  app.querySelector(".window-content").classList.remove("window-content");
+  app.querySelector(".window-header").style.backgroundColor =
+    "rgba(0, 0, 0, 0.5)";
+  app.querySelector(".window-header").style.border = "0";
+  Array.from(app.querySelectorAll("button")).forEach((el) =>
+    app.querySelector(".window-header").appendChild(el)
+  );
+  app.querySelector(".close").remove();
+};
+
+Hooks.once("socketlib.ready", () => {
+  socket = socketlib.registerModule("everybodylikedthis");
+  socket.register("approval", approval);
+  console.log("The socket is ready to be used.");
+});
+
+
+
+Hooks.once("ready", async () => {
+  cleanWindowClass(module.status);
+  Array.from(
+    module.status.element[0].querySelectorAll(".module-control")
+  ).forEach((el) =>
+    el.addEventListener("click", (event) => {
+      sendApproval(event.currentTarget.dataset.approval === "true");
+    })
+  );
+});
+
+const approval = (user, approval) => {
+  let newElement = document.createElement("div");
+  //Add transition styles to the new element
+  newElement.style.transition = "opacity 1s";
+  newElement.style.paddingLeft = "8px";
+
+  newElement.innerHTML = `<p>${user} ${
+    approval ? "approves" : "disapproves"
+  }.</p>`;
+  module.status.element[0].querySelector(".statusbox").appendChild(newElement);
+
+  setTimeout(() => {
+    newElement.style.opacity = 0;
+    setTimeout(() => {
+      newElement.remove();
+    }, 1000);
+  }, 5000);
+};
