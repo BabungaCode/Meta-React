8fb6353d80d2bb620d7cca91a099c694cf0b7da5
babungacode <babungagamer@gmail.com>
2025-07-13 14:40:59 -0700

    memory update

--- scripts/meta-react.js
+++ scripts/meta-react.js
@@ -1,7 +1,18 @@
 let socket;
 let module;
-let imgpos = "15%";
+let metaReactInstance;
 
+// --- Register setting as a per-player token-to-position map ---
+Hooks.once("init", () => {
+  game.settings.register("metareact", "imgPositionsByToken", {
+    name: "Image Positions by Token",
+    hint: "Stores individual image vertical positions per token.",
+    scope: "client",
+    config: false,
+    type: Object,
+    default: {},
+  });
+});
 
 class Status extends Application {
   static get defaultOptions() {
@@ -19,14 +30,24 @@ class Status extends Application {
   }
 }
 
-let metaReactInstance;
+// --- Helper: Get image position for token name ---
+function getImagePositionForToken(tokenName) {
+  const all = game.settings.get("metareact", "imgPositionsByToken") || {};
+  return all[tokenName] || "15%";
+}
+
+// --- Helper: Set image position for token name ---
+function setImagePositionForToken(tokenName, position) {
+  const all = game.settings.get("metareact", "imgPositionsByToken") || {};
+  all[tokenName] = position;
+  game.settings.set("metareact", "imgPositionsByToken", all);
+}
 
 class MetaReact {
   constructor() {
     metaReactInstance = this;
 
     Hooks.once("socketlib.ready", this.onSocketReady.bind(this));
-    Hooks.once("init", this.onInit.bind(this));
     Hooks.once("ready", this.onReady.bind(this));
   }
 
@@ -60,38 +81,46 @@ class MetaReact {
   onSocketReady() {
     socket = socketlib.registerModule("metareact");
     socket.register("approval", approval);
-    console.log("The socket is ready to be used.");
+    console.log("MetaReact | Socket ready.");
   }
 
-  onInit() {
+  onReady() {
     console.log("Meta React | Initialising the module.");
     module = game.modules.get("metareact");
     module.status = new Status();
     module.status.render(true);
-  }
-
-  onReady() {
     this.cleanWindowClass(module.status);
+
+    // Set up UI button listeners
     Array.from(module.status.element[0].querySelectorAll(".module-control")).forEach((el) => {
       el.addEventListener("click", (event) => {
         const approvalText = event.currentTarget.dataset.approval;
-        if (approvalText === "settings" ){
-          imgpos = prompt("On a scale of 0% - 100% how far down do you want your img snippet to be.", "15%");
+        const tokenName = this.getCurrentUserName();
+
+        if (approvalText === "settings") {
+          const current = getImagePositionForToken(tokenName);
+          const newPos = prompt("On a scale of 0% - 100% how far down do you want your image snippet to be?", current);
+          if (newPos) {
+            setImagePositionForToken(tokenName, newPos);
+          }
           return;
-        }else if(approvalText === "custom"){
-          let customText = prompt("How does your character react?", "wont remember this.");
-          this.sendApproval(customText);
+        } else if (approvalText === "custom") {
+          const customText = prompt("How does your character react?", "won't remember this.");
+          if (customText) this.sendApproval(customText);
           return;
         }
+
         this.sendApproval(approvalText);
       });
     });
   }
 
   approval(name, art, approval) {
-    let newContainer = document.createElement('div');
-    let newTitle = document.createElement('div')
-    
+    const pos = getImagePositionForToken(name);
+
+    const newContainer = document.createElement('div');
+    const newTitle = document.createElement('div');
+
     newContainer.style.height = "70px";
     newContainer.style.width = "300px";
     newContainer.style.transition = "opacity 1s";
@@ -99,8 +128,8 @@ class MetaReact {
 
     newContainer.style.backgroundImage = `url(${art})`;
     newContainer.style.backgroundSize = "cover";
-    newContainer.style.backgroundPosition = "100% " + imgpos;
-    
+    newContainer.style.backgroundPosition = `100% ${pos}`;
+
     newTitle.innerHTML = `${name} ${approval}`;
     newTitle.style.position = "absolute";
     newTitle.style.backgroundColor = "#0000005e";
@@ -109,11 +138,9 @@ class MetaReact {
     newTitle.style.left = "0px";
     newTitle.style.width = "300px";
     newTitle.style.padding = "2px";
-  
 
     module.status.element[0].querySelector(".statusbox").appendChild(newContainer);
     newContainer.appendChild(newTitle);
-
 
     setTimeout(() => {
       newContainer.style.opacity = 0;
@@ -125,5 +152,5 @@ class MetaReact {
 new MetaReact();
 
 function approval(name, art, approvalText) {
-  metaReactInstance?.approval(name, art, approvalText)
+  metaReactInstance?.approval(name, art, approvalText);
 }
